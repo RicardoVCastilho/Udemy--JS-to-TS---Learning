7610ab2fda2e59649d5973c562b06366e56c5349
Ricardo Vitor <ricardovdev@hotmail.com>
2024-08-11 17:12:30 -0300

    Adding aulas 9 e 10

---
+++ aula10-tipos-primitivos/index.js
@@ -0,0 +1,5 @@
+const nome = 'Ricardo'; //string
+const num1 = 12; // number
+let nomeAluno; //undefined -> Não aponta para nada na memória (normalmente retorno de erros)
+const sobrenomeAluno = null; // Nulo -> Não aponta para nada na memória (Deliberadamente quero que o valor seja nulo, caso não tenha nada selecionado, por ex)
+const boolean = true; // boolean -> true or false (0,1) - Lógico
---
+++ aula9-var-vs-let/index.js
@@ -0,0 +1,6 @@
+// ECMAScript = padronização do JS feito pela empresa ECMA
+
+var nome = 'Ricardo';
+var nome = 'Vitor';
+
+console.log(nome);
